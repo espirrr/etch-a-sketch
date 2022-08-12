81f642108c6b95f7a460bfde262ed3837fba5b9a
Espir <e.ingasalo@gmail.com>
2022-08-11 21:42:46 -1000

    Squares now turn black once the mouse enters each box. Prompt button is working but need to make it change the grid once the dimensions are chosen.

--- draw.js
+++ draw.js
@@ -1,10 +1,19 @@
 const gridContainer = document.querySelector('#gridContainer');
+const typeGridDimensions = document.querySelector('#selectDimensions');
+var chosenDimensions = 10;
 
+typeGridDimensions.addEventListener('click', function() {
 
-var gridDimensions = 25;
+    chosenDimensions = prompt("Please choose a number under 100.");
+    alert("Your grid will be" + " " + chosenDimensions + " " + "x" + " " + chosenDimensions);
 
-// gridContainer.textContent = "gridContainer";
+});
 
+console.log(gridDimensions);
+var gridDimensions = chosenDimensions;
+console.log(gridDimensions);
+
+// Creates rows depending on the # of dimensions chosen.
 for (i = 0; i < gridDimensions; i++) {
     
     gridContainer.innerHTML += '<div class="row"></div>'
@@ -13,7 +22,7 @@ for (i = 0; i < gridDimensions; i++) {
 
 const gridRows = document.querySelectorAll('.row');
 
-
+// Creates columns in each row depending on the # of dimensions chosen.
 for (i = 0; i < gridDimensions; i++) {
     
     for (j = 0; j < gridDimensions; j++) {
@@ -21,5 +30,30 @@ for (i = 0; i < gridDimensions; i++) {
         gridRows[j].innerHTML += '<div class="column">  </div>';
 
     }
-    
 }
+
+var gridSquares = document.querySelectorAll('.column');
+
+// Creates an array from all of the squares with class .column
+var gridArray = Array.from(gridSquares);
+
+
+var isMouseDown = false;
+
+// Each element in the array will have its own addEventListener by giving each index variable "uniqueSquare".
+for (k = 0; k < gridArray.length; k++) {
+
+    var uniqueSquare = gridArray[k];
+    
+
+    uniqueSquare.addEventListener('mouseenter', function(e) {
+
+        e.target.style.background = 'black';
+    
+    });
+
+}
+
+
+
+
